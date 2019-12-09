5e29c87586c300ba6a6df5c1b312b74b9273e47a
duwon <duwon@duwon.net>
2019-12-10 01:03:12 +0900

    Sensor 0,1,2 입력 구현 DC3.3V, 12V 제어 구현

--- Src/lpsb.c
+++ Src/lpsb.c
@@ -33,30 +33,45 @@ void printUartLogo(void)
 
 uint8_t getID(void)
 {
-    //return GPIOA->IDR;
-    uint8_t iID = 0;
-    iID = HAL_GPIO_ReadPin(DIP6_GPIO_Port,DIP6_Pin);
-    iID = (iID<<1) + HAL_GPIO_ReadPin(DIP5_GPIO_Port,DIP5_Pin);
-    iID = (iID<<1) + HAL_GPIO_ReadPin(DIP4_GPIO_Port,DIP4_Pin);
-    iID = (iID<<1) + HAL_GPIO_ReadPin(DIP3_GPIO_Port,DIP3_Pin);
-    iID = (iID<<1) + HAL_GPIO_ReadPin(DIP2_GPIO_Port,DIP2_Pin);
-    iID = (iID<<1) + HAL_GPIO_ReadPin(DIP1_GPIO_Port,DIP1_Pin);
-    iID = (iID<<1) + HAL_GPIO_ReadPin(DIP0_GPIO_Port,DIP0_Pin);
+  //return GPIOA->IDR;
+  uint8_t iID = 0;
+  iID = HAL_GPIO_ReadPin(DIP6_GPIO_Port, DIP6_Pin);
+  iID = (iID << 1) + HAL_GPIO_ReadPin(DIP5_GPIO_Port, DIP5_Pin);
+  iID = (iID << 1) + HAL_GPIO_ReadPin(DIP4_GPIO_Port, DIP4_Pin);
+  iID = (iID << 1) + HAL_GPIO_ReadPin(DIP3_GPIO_Port, DIP3_Pin);
+  iID = (iID << 1) + HAL_GPIO_ReadPin(DIP2_GPIO_Port, DIP2_Pin);
+  iID = (iID << 1) + HAL_GPIO_ReadPin(DIP1_GPIO_Port, DIP1_Pin);
+  iID = (iID << 1) + HAL_GPIO_ReadPin(DIP0_GPIO_Port, DIP0_Pin);
 
-    return iID;
+  return iID;
 }
 
 uint32_t getBATLevel(void)
 {
-    if (HAL_ADCEx_Calibration_Start(&hadc, ADC_SINGLE_ENDED) != HAL_OK)
-    {
-        Error_Handler();
-    }
+  if (HAL_ADCEx_Calibration_Start(&hadc, ADC_SINGLE_ENDED) != HAL_OK)
+  {
+    Error_Handler();
+  }
 
-    HAL_ADC_Start(&hadc);
-    HAL_ADC_PollForConversion(&hadc, 100);
+  HAL_ADC_Start(&hadc);
+  HAL_ADC_PollForConversion(&hadc, 100);
 
-    return HAL_ADC_GetValue(&hadc);
+  return HAL_ADC_GetValue(&hadc);
+}
+
+void controlDCOn(DC_Pin pin)
+{
+  HAL_GPIO_WritePin(CONTROL_DC12V_GPIO_Port, pin, GPIO_PIN_SET);
+}
+
+void controlDCOff(DC_Pin pin)
+{
+  HAL_GPIO_WritePin(CONTROL_DC12V_GPIO_Port, pin, GPIO_PIN_RESET);
+}
+
+GPIO_PinState lpsb_GetSensor(Sensor_Pin pin)
+{
+  return HAL_GPIO_ReadPin(SENSOR0_GPIO_Port, 1 << pin);
 }
 
 void lpsb_start(void)
@@ -66,9 +81,18 @@ void lpsb_start(void)
 
   uint8_t lpsb_ID = getID();
   uint32_t lpsb_BATLevel = getBATLevel();
-  
+
   printf("ID: 0x%x\r\n", lpsb_ID);
   printf("BAT Level: %d\r\n", lpsb_BATLevel);
+
+  controlDCOn(DC12V);
+  controlDCOn(DC3V);
+
+  uint8_t sen0 = lpsb_GetSensor(SEN0);
+  uint8_t sen1 = lpsb_GetSensor(SEN1);
+  uint8_t sen2 = lpsb_GetSensor(SEN2);
+
+  printf("Sensor %d, %d, %d\r\n", sen0, sen1, sen2);
 }
 
 void lpsb_while(void)
